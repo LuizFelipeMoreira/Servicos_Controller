f208e540dd9d0b4fdcbed7baf96ba81bb454db75
luiz-felipe-moreira <luizfelipemlds65@gmail.com>
2024-01-09 23:36:53 -0300

    tipando props

--- src/components/Form/Input.jsx
+++ src/components/Form/Input.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 const Inputs = styled.input`
   padding: 8px;
@@ -10,13 +11,44 @@ const Inputs = styled.input`
   border: 1px solid black;
 `;
 
-const Input = ({ type, id, name, label, width, classe }) => {
+const Input = ({
+  type,
+  id,
+  name,
+  label,
+  width,
+  classe,
+  data,
+  setValue,
+  value,
+}) => {
   return (
     <div className={classe}>
       <label htmlFor={id}>{label}</label>
-      <Inputs type={type} id={id} name={name} style={{ width: `${width}%` }} />
+      {value}
+      <Inputs
+        type={type}
+        id={id}
+        name={name}
+        sett
+        value={value}
+        onChange={handleChange}
+        style={{ width: `${width}%` }}
+      />
     </div>
   );
+};
+
+Input.propTypes = {
+  type: PropTypes.string,
+  id: PropTypes.string,
+  name: PropTypes.string,
+  label: PropTypes.string,
+  width: PropTypes.number,
+  classe: PropTypes.string,
+  data: PropTypes.object,
+  setValue: PropTypes.func,
+  value: PropTypes.string,
 };
 
 export default Input;
